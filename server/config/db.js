5ddd0ab65c2c7aa4ec3c966421a80ab5ae7d68a2
Muqadas Nadeem <muqadasnadeem143@gmail.com>
2026-09-24 05:29:03 -0700

    fix db connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) return;
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    isConnected = db.connections[0].readyState === 1;
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error.message);
-    process.exit(1);
+    console.error("MongoDB error:", error.message);
   }
 };
 
